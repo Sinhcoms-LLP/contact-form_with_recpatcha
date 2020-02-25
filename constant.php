163281e7c380ae703289fd03b5ddabda3be99e8c
SINHCOMS LLP <55934694+Sinhcoms-LLP@users.noreply.github.com>
2020-02-26 03:48:36 +0530

    Add files via upload

---
+++ constant.php
@@ -0,0 +1,5 @@
+<?php
+//reCAPTCHA Configuration - go to Google and get the below keys http://www.google.com/recaptcha/admin
+define('SITE_KEY',"6LfndcMUAAAAAKBssqsbUddtomAESyJVYytsYiAT"); 
+define('SECRET_KEY',"6LfndcMUAAAAALrM-S4IPAN--pEcf_McyUJHsx5_");
+?>
